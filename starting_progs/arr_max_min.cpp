8eb63a3305947e0002f605cc2b03d5ad7225c644
Vladislav <41309611+nikomorphine@users.noreply.github.com>
2018-07-18 02:03:43 +0300

    Add files via upload

---
+++ starting_progs/arr_max_min.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int max_elem(int * p, int * q){
+	int max = *p;
+	for (; p != q; ++p)
+		if (*p > max)
+			max = *p;
+
+	return max;
+}
+
+int min_elem(int * p, int * q){
+	int min = *p;
+	for (; p != q; ++p)
+		if (*p < min)
+			min = *p;
+
+	return min;
+}
+
+int main(){
+	int size = 0;
+	cin >> size;
+	int arr[size] = {};
+	int temp = 0;
+	for (int i = 0; i < size; ++i){
+		cin >> temp;
+		arr[i] = temp;
+	}
+	cout << max_elem(arr, arr + size) << endl <<  min_elem(arr, arr + size) << endl;
+
+	return 0;
+}
